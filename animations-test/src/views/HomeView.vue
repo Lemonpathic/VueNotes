b706003e17891f9731fd1a2c78271ca695b79a1f
Lemonpathic <goofymanbeek2.0@gmail.com>
2024-05-07 21:22:28 -0400

    Working on animations

---
+++ animations-test/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from "vue";
+import {useRoute, useRouter} from "vue-router";
+
+const router = useRouter();
+const route = useRoute()
+
+const search = ref("");
+
+const people = ref([])
+
+const addPerson = () => {
+  if (search.value.trim() !== "") {
+    const person = {
+      name: search.value
+    }
+    people.value.push(person)
+    search.value = ""
+  }
+}
+
+const addInfo = () => {
+
+}
+
+</script>
+
+<template>
+  <input type="text" placeholder="Add Person..." v-model.trim="search" @keyup.enter="addPerson">
+  <h1 v-for="person in people" @click="">{{ person }}</h1>
+</template>
+
+<style scoped>
+
+</style>
